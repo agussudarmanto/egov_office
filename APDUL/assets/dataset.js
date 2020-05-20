0d94cc3ee07751406bb8df9c2344ccae3e00847c
Agus Sudarmanto <agus.sudarmanto@gmail.com>
2020-05-20 09:50:14 +0700

    First commit of APDUL
    APDUL - Aplikasi Data Usulan Satya Lencana
    
    Aplikasi Data Usulan Satya Lencana atau disingkat APDUL merupakan aplikasi yang
    digunakan untuk mengelola data usulan penghargaan Satya Lencana. APDUL versi 0.1 dapat
    menampilkan data dan dokumen elektronik pegawai yang diusulkan penghargaan, dokumen
    elektronik yang ditampilkan pada aplikasi ini yaitu:
    - DRH
    - SK CPNS
    - SK PNS
    - SK JABATAN
    - SK PANGKAT

---
+++ APDUL/assets/dataset.js
@@ -0,0 +1,37 @@
+let dataSet = [
+  [
+    "1",
+    "196900001990001002",
+    "Rudi Soepadi, S.H., M.Hum.",
+    "Kepala Bagian",
+    "Pembina Tingkat I",
+  ],
+  [
+    "2",
+    "196900001990001002",
+    "Heni Sukmana, S.H., M.H.",
+    "Kepala Bagian",
+    "Pembina",
+  ],
+  [
+    "3",
+    "196900001990001002",
+    "Seno",
+    "Pengadministrasi Umum",
+    "Penata Muda Tingkat I",
+  ],
+  [
+    "4",
+    "196900001990001002",
+    "April",
+    "Pengadministrasi Umum",
+    "Penata Tingkat I",
+  ],
+  [
+    "5",
+    "196900001990001002",
+    "Mimin",
+    "Pengadministrasi Umum",
+    "Penata Muda Tingkat I",
+  ],
+];
